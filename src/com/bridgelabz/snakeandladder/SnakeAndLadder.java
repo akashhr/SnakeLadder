31f3a340d4c269c756703c9f1abdae582d8415a3
Akash-gowda-hr <akashhr2040@gmail.com>
2021-08-23 17:25:02 +0530

    ADDED: dice roll feature

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -3,5 +3,8 @@ package com.bridgelabz.snakeandladder;
 public class SnakeAndLadder {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the snake and ladder game!!!!!!!!!!!!!");
+		int diceNumber = 1 + (int) (Math.random() * 6);
+		System.out.println(diceNumber);
+		
 	}
 }
