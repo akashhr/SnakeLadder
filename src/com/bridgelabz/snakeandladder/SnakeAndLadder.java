c11fec285c44804ff935c93822a81464e11f16f0
Akash-gowda-hr <akashhr2040@gmail.com>
2021-08-23 17:20:54 +0530

    ADDED:Welcome message

---
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.snakeandladder;
+
+public class SnakeAndLadder {
+	public static void main(String[] args) {
+		System.out.println("Welcome to the snake and ladder game!!!!!!!!!!!!!");
+	}
+}
