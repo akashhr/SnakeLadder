b3259264c53fa44259805477eed94d60a7c7c5f8
Akash-gowda-hr <akashhr2040@gmail.com>
2021-08-23 17:28:22 +0530

    ADDED: starting position of A

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -3,6 +3,7 @@ package com.bridgelabz.snakeandladder;
 public class SnakeAndLadder {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the snake and ladder game!!!!!!!!!!!!!");
+		int startPOsitionA = 0;
 		int diceNumber = 1 + (int) (Math.random() * 6);
 		System.out.println(diceNumber);
 		
